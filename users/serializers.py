6421658437a8fdc0548eac5b9517a79b9ec4db4e
lonaxit <lonaxit@outlook.com>
2022-08-01 20:06:55 +0100

    Added serializer to manage  update user password

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from dataclasses import fields
+import profile
 from rest_framework import serializers
+from core.models  import Profile
 
 # IMPORT CUSTOM USER
 from django.contrib.auth import get_user_model
@@ -8,9 +10,25 @@ User = get_user_model()
 # user app
 class UserSerializer(serializers.ModelSerializer):
     
+    # profile = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
         fields = ('id','username','first_name','last_name','other_name','is_employee','is_account','is_normal')
+        
+# 
+class UpdateUserPasswordSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('id','password')
+
+
+class UpdatePasswordWithUsernameSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('id','password','username')
         
 
 
