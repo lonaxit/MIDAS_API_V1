e11b4bb12613ab1e85189a63224228d0eb4183f1
lonaxit <lonaxit@outlook.com>
2022-08-01 20:08:12 +0100

    Added urls for updating user password

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,12 @@ from .views import *
 urlpatterns=[
     path('register/',RegistrationView.as_view()),
     path('me/',RetrieveUserView.as_view()),
-    path('all/users/',retrieveAllUsers.as_view(), name="all-users"),   
+    path('all/users/',retrieveAllUsers.as_view(), name="all-users"),
+    path('<int:pk>/user-update/',UpdateUser.as_view(), name="update-user"),
+    
+    # change password using userid
+    path('<int:pk>/update-password/',UpdateUserPassword.as_view(), name="update-password"),
+    
+    # Change password using username
+    path('username-update-password/',UpdatePasswordUsername.as_view(), name="username-update-password"),     
 ]
