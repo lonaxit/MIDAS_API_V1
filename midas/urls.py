2647ab2c3536375b38869d327f88e995bb5462ab
lonaxit <lonaxit@outlook.com>
2022-08-02 21:08:41 +0100

    Added drf spectacular views

--- midas/urls.py
+++ midas/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 
@@ -29,5 +30,11 @@ urlpatterns = [
     # custom registration app
      path('auth/', include('users.urls')),
      path('api/v1/',include('profiles.api.urls')),
-     path('api/v1/',include('cooperators.api.urls'))
+     path('api/v1/',include('cooperators.api.urls')),
+     
+    #  docs
+     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
