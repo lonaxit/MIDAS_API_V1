b13f04a91e7bd5d521d747bc748d92d1dcd203e5
lonaxit <lonaxit@outlook.com>
2022-08-02 20:45:25 +0100

    removed code

--- midas/urls.py
+++ midas/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+
+
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('api/v1/',include('djoser.urls')),
     path('api/v1/',include('djoser.urls.authtoken')),
