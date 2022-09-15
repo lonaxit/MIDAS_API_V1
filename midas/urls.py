e24dae12c5e9a12e20d9abdac7d1ba3a2a4363ef
lonaxit <lonaxit@outlook.com>
2022-09-15 22:01:10 +0100

    Update urls.py
    Removed spacing

--- midas/urls.py
+++ midas/urls.py
@@ -18,9 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
-
-
-
 urlpatterns = [
     
     path('admin/', admin.site.urls),
