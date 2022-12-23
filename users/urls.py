8b41ce56cdffcfe136578547e355fc997429cdeb
lonaxit <lonaxit@outlook.com>
2022-12-23 18:34:19 +0100

    Update urls.py
    Added url names

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('register/',RegistrationView.as_view()),
-    path('me/',RetrieveUserView.as_view()),
+    path('register/',RegistrationView.as_view(),name="register"),
+    path('me/',RetrieveUserView.as_view(), name="me"),
+    path('get-user/<str:username>/', GetUserWithUsername.as_view(), name="get-user"),
     path('all/users/',retrieveAllUsers.as_view(), name="all-users"),
     path('<int:pk>/user-update/',UpdateUser.as_view(), name="update-user"),
     
