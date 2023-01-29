35de9fbba8fccdd1cd2256103d8e5b45522582d7
lonaxit <lonaxit@outlook.com>
2023-01-29 15:20:54 +0100

    Update views.py
    Added a new depositStatement view

--- cooperators/api/views.py
+++ cooperators/api/views.py
@@ -27,6 +27,32 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+# user saving statement
+
+#  find statement of saving given a user id and date range start and end dates 
+class DepositStatement(generics.ListAPIView):
+    serializer_class = SavingSerializer
+    queryset = Saving.objects.all()
+    permission_classes =[IsAuthenticated & IsAuthOrReadOnly]
+    
+    def get_queryset(self):
+        
+        # pk = self.kwargs['pk']
+        start = self.kwargs['startdate']
+        end = self.kwargs['enddate']
+        try:
+            user = self.request.user
+            Savings = Saving.objects.filter(user=user,transaction_date__gte=start,transaction_date__lte=end)
+            
+            if not Savings:
+                raise ValidationError('No savings or search criteria not fulfilled')
+            return Savings
+        
+        except User.DoesNotExist:          
+
+            raise ValidationError('User Does Not exist')
+
+
 
 # my loans
 class userLoans(generics.ListAPIView):
