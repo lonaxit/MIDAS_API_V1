1ea7da3513cb7df2a9a212ebc69a7e286bf3504a
lonaxit <lonaxit@outlook.com>
2023-01-29 15:20:17 +0100

    Update urls.py
    Added  a new url path

--- cooperators/api/urls.py
+++ cooperators/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
      
     path('userbalances/<str:startdate>/<str:enddate>/',userLoanBalancesByDate.as_view(),name="user-balances"),
     path("myloans/", userLoans.as_view(), name="my-loans"),
+    path('<str:startdate>/<str:enddate>/Depositstatement/',DepositStatement.as_view(),name="deposit-statement"),
  ]
