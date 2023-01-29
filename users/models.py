58c70c31c6c0b6cbab62f5e6761753a73f89b4bd
lonaxit <lonaxit@outlook.com>
2023-01-29 15:24:29 +0100

    Update models.py
    Created user creation validation

--- users/models.py
+++ users/models.py
@@ -7,19 +7,32 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class CustomUserManager(BaseUserManager):
     
-    def create_user(self, first_name,last_name,username,password=None):
+    def create_user(self, first_name,last_name,username,ippis_number,dob,dofa,password=None):
         
         if not username:
-            raise ValueError('Users must have a username and must be unique')
+            raise ValueError('User must have a username and must be unique')
         if not first_name:
-            raise ValueError('Users must have a first name')
+            raise ValueError('User must have a first name')
         if not last_name:
-            raise ValueError('Users must have a last name')
+            raise ValueError('User must have a last name')
+        if not ippis_number:
+            raise ValueError('Please enter an IPPIS NUMBER')
+        if ippis_number and isinstance(ippis_number,str):
+            raise ValueError('Ippis Number can not be a string')
+        if not dob:
+            raise ValueError('User must have a date of birth')
+        if not dofa:
+            raise ValueError('User must have a date of first appointment')
+             
+        
 
         user = self.model(
             username=username,
             first_name=first_name,
-            last_name = last_name
+            last_name = last_name,
+            ippis_number = ippis_number,
+            dob = dob,
+            dofa=dofa
         )
 
         user.set_password(password)
@@ -30,29 +43,29 @@ class CustomUserManager(BaseUserManager):
         return user
 
     # method to create other roles
-    def create_employee(self, first_name,last_name,username,password=None):
-        user = self.create_user(first_name,last_name,username,password)
+    def create_employee(self, first_name,last_name,username, ippis_number,dob,dofa,password=None):
+        user = self.create_user(first_name,last_name,username,ippis_number,dob,dofa,password)
         user.is_employee=True
         user.save()
         
         return user
     
-    def create_account(self, first_name,last_name,username,password=None):
-        user = self.create_user(first_name,last_name,username,password)
+    def create_account(self, first_name,last_name,username,ippis_number,dob,dofa,password=None):
+        user = self.create_user(first_name,last_name,username,ippis_number,dob,dofa,password)
         user.is_account=True
         user.save()
         
         return user
     
-    def create_normal(self, first_name,last_name,username,password=None):
-        user = self.create_user(first_name,last_name,username,password)
+    def create_normal(self, first_name,last_name,username,ippis_number,dob,dofa,password=None):
+        user = self.create_user(first_name,last_name,username,ippis_number,dob,dofa,password)
         user.is_normal=True
         user.save()
         
         return user
     
-    def create_superuser(self, first_name,last_name,username,password=None):
-        user = self.create_user(first_name,last_name,username,password)
+    def create_superuser(self, first_name,last_name,username,ippis_number,dob,dofa,password=None):
+        user = self.create_user(first_name,last_name,username,ippis_number,dob,dofa,password)
       
         user.is_superuser = True
         user.is_staff =True
@@ -66,9 +79,11 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     other_name = models.CharField(max_length=255,blank=True)
-    # dob = models.DateField(blank=True,null=True)
+    ippis_number = models.BigIntegerField(default=0000000)
+    dob = models.DateField(default="2000-01-02")
+    dofa = models.DateField(default="2000-01-02")
     # employment_type = models.CharField(max_length=100, null=True,blank=True)
-    # ippis_number = models.BigIntegerField(null=True, blank=True)
+    
     # home_address = models.CharField(max_length=500, null=True,blank=True)
     # gender = models.CharField(max_length=20, null=True,blank=True)
     # job_cadre = models.CharField(max_length=100,null=True,blank=True)
@@ -80,7 +95,7 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     # nok_relationship = models.CharField(max_length=50, null=True,blank=True)
     # bank = models.CharField(max_length=50, null=True,blank=True)
     # bank_account= models.CharField(max_length=20, null=True,blank=True)
-    # avatar = models.ImageField(null=True,blank=True)
+    avatar = models.ImageField(null=True,blank=True)
     date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -92,7 +107,7 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     objects = CustomUserManager()
     
     USERNAME_FIELD ='username'
-    REQUIRED_FIELDS = ['first_name','last_name']
+    REQUIRED_FIELDS = ['first_name','last_name','ippis_number','dob','dofa']
     
     def __str__(self):
         return self.username
