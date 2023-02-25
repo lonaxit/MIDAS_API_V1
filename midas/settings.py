46eea75420007196388cce9a60cc5a063a3d69a8
lonaxit <lonaxit@outlook.com>
2023-02-25 13:43:50 +0100

    Update settings.py
    Added CORS_ORIGIN_WHITELIST

--- midas/settings.py
+++ midas/settings.py
@@ -71,9 +71,11 @@ INSTALLED_APPS = [
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
-
-       'http://localhost:8080',
-        'http://localhost:8081',
+    'https://midastouchonline.co/',
+    'https://www.midastouchonline.co/',
+    'https://midas-frontend.onrender.com/',
+    'http://localhost:8080',
+    'http://localhost:8081',
 )
 
 # config
