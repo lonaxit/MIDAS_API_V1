852f979ee2b93389b2fac1c9048b80d199851fed
lonaxit <lonaxit@outlook.com>
2023-04-15 13:00:44 +0100

    Update tasks.py
    Updated task

--- core/tasks.py
+++ core/tasks.py
@@ -4,6 +4,7 @@ from django.db import transaction
 User = get_user_model()
 from rest_framework.exceptions import ValidationError
 import math
+import io, csv, pandas as pd
 
 @shared_task(bind=True)
 def fun(self):
@@ -15,42 +16,82 @@ def fun(self):
 def mul(x, y):
     return x * y
 
-@shared_task
-def create_loan_subscription(dtframe,request):
+# @shared_task
+# def create_loan_subscription(dtframe,request):
     
-    try:
+#     try:
                  
-        for dtframe in dtframe.itertuples():
-            guarantor_id1 = 0
-            guarantor_id2 = 0
+#         for dtframe in dtframe.itertuples():
+#             guarantor_id1 = 0
+#             guarantor_id2 = 0
                     
-            if not math.isnan(dtframe.guarantor_id1):
+#             if not math.isnan(dtframe.guarantor_id1):
                 
-                guarantor_id1 = dtframe.guarantor_id1
+#                 guarantor_id1 = dtframe.guarantor_id1
                 
-            elif not math.isnan(dtframe.guarantor_id2):
+#             elif not math.isnan(dtframe.guarantor_id2):
                 
-                guarantor_id2 = dtframe.guarantor_id2
+#                 guarantor_id2 = dtframe.guarantor_id2
+                    
+#             Loan.objects.create(
+#                         loan_date = dtframe.disbursement_date,
+#                         start_date = dtframe.loan_start_date,
+#                         end_date = dtframe.loan_end_date,
+#                         active = dtframe.loan_status,
+#                         transaction_code = dtframe.ref.replace('-', ''), 
+#                         sub_id = int(dtframe.id),
+#                         applied_amount= float(dtframe.amount_applied),
+#                         approved_amount = float(dtframe.amount_approved),
+#                         monthly_deduction = float(dtframe.monthly_deduction),
+#                         net_pay= float(0.00),
+#                         tenor = int(dtframe.custom_tenor),
+#                         created_by = request.user,
+#                         product= Product.objects.get(pk=int(dtframe.product_id)),
+#                         owner = User.objects.get(pk = int(dtframe.user_id)),
+#                         guarantor_one= guarantor_id1,
+#                         guarantor_two = guarantor_id2,
+#                     )
+                  
+#     except Exception as e:
+        
+#         raise ValidationError(e)
+    
+    
+@shared_task
+def create_loan_subscription(excel_data, request):
+    reader = pd.read_excel(excel_data)
+    data_frame = reader
+    try:
+        for row in data_frame.itertuples():
+            guarantor_id1 = None
+            guarantor_id2 = None
+            
+            if row.guarantor_id1 is not None:
+                guarantor_id1 = row.guarantor_id1
+                
+            elif row.guarantor_id2 is not None:
+                guarantor_id2 = row.guarantor_id2
                     
             Loan.objects.create(
-                        loan_date = dtframe.disbursement_date,
-                        start_date = dtframe.loan_start_date,
-                        end_date = dtframe.loan_end_date,
-                        active = dtframe.loan_status,
-                        transaction_code = dtframe.ref.replace('-', ''), 
-                        sub_id = int(dtframe.id),
-                        applied_amount= float(dtframe.amount_applied),
-                        approved_amount = float(dtframe.amount_approved),
-                        monthly_deduction = float(dtframe.monthly_deduction),
-                        net_pay= float(0.00),
-                        tenor = int(dtframe.custom_tenor),
-                        created_by = request.user,
-                        product= Product.objects.get(pk=int(dtframe.product_id)),
-                        owner = User.objects.get(pk = int(dtframe.user_id)),
-                        guarantor_one= guarantor_id1,
-                        guarantor_two = guarantor_id2,
-                    )
+                loan_date=row.disbursement_date,
+                start_date=row.loan_start_date,
+                end_date=row.loan_end_date,
+                active=row.loan_status,
+                transaction_code=row.ref.replace('-', ''), 
+                sub_id=row.id,
+                applied_amount=row.amount_applied,
+                approved_amount=row.amount_approved,
+                monthly_deduction=row.monthly_deduction,
+                net_pay=0.0,
+                tenor=row.custom_tenor,
+                created_by=request.user,
+                product=Product.objects.get(pk=row.product_id),
+                owner=User.objects.get(pk=row.user_id),
+                guarantor_one=guarantor_id1,
+                guarantor_two=guarantor_id2,
+            )
                   
-    except Exception as e:
-        
-        raise ValidationError(e)
+    except ValueError as e:
+        raise ValueError(f"Invalid value: {e}")
+    except TypeError as e:
+        raise TypeError(f"Type error: {e}")
