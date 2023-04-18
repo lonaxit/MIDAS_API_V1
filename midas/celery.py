d7be39c4bb3fad2e253c0c37f13e0bb3777ed223
lonaxit <lonaxit@outlook.com>
2023-04-18 10:17:47 +0100

    Update celery.py
    updated

--- midas/celery.py
+++ midas/celery.py
@@ -2,9 +2,8 @@ from __future__ import absolute_import,unicode_literals
 import os
 
 from celery import Celery
-# from django.conf import settings
-# from core import tasks
-# from core.tasks import create_loan_subscription, mul
+from django.conf import settings
+
 
 
 # Set the default Django settings module for the 'celery' program.
@@ -19,8 +18,8 @@ app = Celery('midas')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
-# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# app.autodiscover_tasks()
 
 
 @app.task(bind=True)
