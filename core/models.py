800bab247097d9cf8332d21f563aa790c692cc59
lonaxit <lonaxit@outlook.com>
2023-04-19 14:35:12 +0100

    Update models.py
    Removed commented out code

--- core/models.py
+++ core/models.py
@@ -89,8 +89,6 @@ class MasterLoanDeduction(models.Model):
     active = models.BooleanField(default=True)
     transaction_code = models.BigIntegerField()
     cumulative_amount = models.DecimalField(max_digits=20,decimal_places=2)
-    # balance = models.DecimalField(max_digits=20,decimal_places=2,null=True)
-    
     created_by = models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name='masterloandeduction')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
