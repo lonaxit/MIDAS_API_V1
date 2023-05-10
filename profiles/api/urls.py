19f330638fec361601fa7de4fbaa7857f2346602
lonaxit <lonaxit@outlook.com>
2023-05-10 20:44:30 +0100

    Update urls.py
    Added new url

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -5,8 +5,8 @@ from profiles.api.views import *
 urlpatterns =[
     
     path("<int:pk>/profileupdate/", ProfileRetrieveUpdate.as_view(), name="update-profile"),
-    
     path("list-profile/", GetProfileList.as_view(), name="list-profile"),
-    path('<int:user>/profile/',GetProfile.as_view(),name='profile')
+    path('<int:user>/profile/',GetProfile.as_view(),name='profile'),
+    path('user/profile/',UserProfileView.as_view(),name='user-profile'),
 
 ]
