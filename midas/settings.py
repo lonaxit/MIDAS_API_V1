18da489b1e837d7364844cc51cfe03bb6762d56c
lonaxit <lonaxit@outlook.com>
2024-02-11 07:46:46 +0100

    Update settings.py
    Updated db url

--- midas/settings.py
+++ midas/settings.py
@@ -144,21 +144,22 @@ WSGI_APPLICATION = 'midas.wsgi.application'
 # }
 
 # production
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':env('NAME'),
-        'USER':env('USER'),
-        'PASSWORD':env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT':env('PORT'),
-    }
-}
-
-# DATABASES={
-#     'default': dj_database_url.config(env('DB_URL'))
+# VARIABLES ARE CHANGED ONLINE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':env('NAME'),
+#         'USER':env('USER'),
+#         'PASSWORD':env('PASSWORD'),
+#         'HOST': env('HOST'),
+#         'PORT':env('PORT'),
+#     }
 # }
+
+# production
+DATABASES={
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
